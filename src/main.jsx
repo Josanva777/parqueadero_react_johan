9207c1533e976394b8b5568e243aa4dd2d0f1e5d
josetvasco <torovascojosedaniel@gmail.com>
2025-03-25 20:34:09 -0500

    Subiendo login y formulario de registro

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Login from './Login'
+import CreateAccount from './CreateAccount'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login/>
+    <CreateAccount/>
   </StrictMode>,
 )
