943891582d888d6196a63bc26a0964faccf7629a
josetvasco <torovascojosedaniel@gmail.com>
2025-05-01 12:37:44 -0500

    Organización de la funció de alertNotificaction

--- src/helpers/funciones.js
+++ src/helpers/funciones.js
@@ -1,23 +1,13 @@
 import Swal from "sweetalert2";
 
-export function alertNotification(message, success) {
-  if (success) {
-      Swal.fire({
-          title: '¡Exitoso!',
-          text: message,
-          icon: 'success',
-          showConfirmButton: false,
-          timer: 2000
-        });  
-  } else {
-      Swal.fire({
-          title: '¡Fallido!',
-          text: message,
-          icon: 'error',
-          showConfirmButton: false,
-          timer: 2000
-        });          
-  }
+export function alertNotification(title, message, type) {
+    Swal.fire({
+      title: title,
+      text: message,
+      icon: type,
+      showConfirmButton: false,
+      timer: 2000
+    });
 }
 
 export function generateToken() {
