906d60961146054ee2c03a85c650fd5b5bf4dfee
josetvasco <torovascojosedaniel@gmail.com>
2025-05-01 17:17:24 -0500

    Depuración de el archivo con bd

--- src/service/database.js
+++ src/service/database.js
@@ -1,6 +1,6 @@
 export let users = [
-  {user:"sebas", password:"1234", roll:"admin", nombre: "Sebastián"},
-  {user:"johan", password:"1234", roll:"admin", nombre: "Sebastián"},
-  {user:"sebastian", password:"1234", roll:"admin", nombre: "Sebastián"},
-  {user:"jose", password:"1234", roll:"admin", nombre: "Sebastián"},
+  {user:"sebas", password:"1234", roll:"admin"},
+  {user:"johan", password:"1234", roll:"admin"},
+  {user:"sebastian", password:"1234", roll:"admin"},
+  {user:"jose", password:"1234", roll:"admin"},
 ];
