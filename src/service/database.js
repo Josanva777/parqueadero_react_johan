078417086aa6f29be06731a58578ec420e0784e6
josetvasco <torovascojosedaniel@gmail.com>
2025-05-06 18:15:42 -0500

    Restaurando el archivo database.js

---
+++ src/service/database.js
@@ -0,0 +1,30 @@
+export let users = [
+  {
+    "id": 1,
+    "rol": 1,
+    "user": "sebas",
+    "email": "sebas@correo.com",
+    "password": "1234"
+  },
+  {
+    "id": 2,
+    "rol": 1,
+    "user": "johan",
+    "email": "johan@correo.com",
+    "password": "1234"
+  },
+  {
+    "id": 3,
+    "rol": 1,
+    "user": "sebastian",
+    "email": "sebastian@correo.com",
+    "password": "1234"
+  },
+  {
+    "id": 4,
+    "rol": 1,
+    "user": "jose",
+    "email": "jose@correo.com",
+    "password": "1234"
+  }
+]
