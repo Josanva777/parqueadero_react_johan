dc30f64cdf6ca57f7920dabcbd53d683e2298c1c
sebastianJH21 <sjimnezc@cesde.net>
2025-05-24 19:23:37 -0500

    Actualizacion de la forma de impresasion de los objetos de la tabla

--- src/routes/RouterApp.jsx
+++ src/routes/RouterApp.jsx
@@ -3,7 +3,7 @@ import RutaProtegida from '../components/RutaProtegida.jsx';
 import NotFound from '../pages/NotFound.jsx';
 import CreateAccount from '../pages/CreateAccount.jsx';
 import TableExample from '../pages/TableExample.jsx';
-import MonthlyPayment from '../pages/MonthlyPayment.jsx';
+// import MonthlyPayment from '../pages/MonthlyPayment.jsx';
 import Blank from '../pages/Blank.jsx';
 import Disponibilidad from '../pages/Disponibilidad.jsx';
 import Entrada from '../pages/Entrada.jsx';
@@ -59,10 +59,10 @@ export let routes = [
     path: '/tablas',
     element: <RutaProtegida protect={<TableExample />} />,
   },
-  {
-    path: '/mensualidad',
-    element: <MonthlyPayment />,
-  },
+  // {
+  //   path: '/mensualidad',
+  //   element: <MonthlyPayment />,
+  // },
   {
     path: '/blank',
     element: <Blank />,
