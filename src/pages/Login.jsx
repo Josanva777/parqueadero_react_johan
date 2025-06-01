f35ea760f0a53ac41fc73717adcef9cddd3775c9
sebasvasquez80 <sebasvb20@gmail.com>
2025-06-01 13:43:59 -0500

    Se corrige conflicto en login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,9 @@
-import fondo from '../../public/fondo.jpg';
 import { useState } from 'react';
 import { alertNotification } from '../helpers/funciones.js';
 import { useNavigate } from 'react-router-dom';
-import { users } from '../service/database.js';
 import imagenes from '../assets/img/imagenes.js';
-const apiUsers = 'http://localhost:3000/users';
+import '../pages/Login.css';
+const apiLogin = 'https://backend-parqueadero-j8gj.onrender.com/api/auth/login';
 
 function Login() {
   let redirection = useNavigate();
@@ -46,7 +45,7 @@ function Login() {
     <section className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
       <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
         <div className="flex flex-col overflow-y-auto md:flex-row">
-          <figure className="h-32 md:h-auto md:w-1/2 colorpicture">
+          <figure className="h-32 md:h-auto md:w-1/2 colorlogo">
             <img
               aria-hidden="true"
               className="object-cover w-full h-full"
